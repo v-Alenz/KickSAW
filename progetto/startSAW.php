8c7dc30df97147ed44bd81ab333fb26d6a375b84
Nicola <nicoinca00@hotmail.it>
2022-01-14 16:41:58 +0100

    cambiate scritte

--- progetto/startSAW.php
+++ progetto/startSAW.php
@@ -2,7 +2,7 @@
 <html lang="it">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> StartSAW </title>
+    <title> StarterPunch! </title>
     <link rel="stylesheet" type="text/css" href="style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+
 <?php
 include $_SERVER['DOCUMENT_ROOT']."/progetto/common/navbar.php";
 ?>
@@ -20,16 +20,16 @@ include $_SERVER['DOCUMENT_ROOT']."/progetto/common/navbar.php";
     <div class= "container sfondopagprincipale">
         <div class="row">
             <div class="col-2">
-                <h1>Coca<br>puzzo!</h1>
-                <p>aaaaaaaaaaaaaaa</p>
-                <a href="/progetto/elencoprogetti.php" class="btn">Esplora Ora &#8594;</a>
+                <h1> Starter Punch! </h1>
+                <p>Dove i migliori progetti prendono vita</p>
+                <a href="/progetto/elencoprogetti.php" class="btn">Scoprili Ora &#8594;</a>
             </div>
             <div class="col-2">
-                <img src="/progetto/immagini/gabibbo.jpg" alt="" class="img">
+                <img src="/progetto/immagini/informatics.jpg" alt="computer in rete" class="img">
             </div>
         </div>
     </div>
-    
+
 </div>
 
 <?php
